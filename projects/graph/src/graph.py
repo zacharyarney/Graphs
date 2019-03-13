0fd87d2d681a90d0696a4bab50d38b5dbf6b9464
zacharyarney <zachary.arney@gmail.com>
2019-03-13 16:29:52 -0700

    adds recursive dft and bfs path methods to graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -30,18 +30,18 @@ class Graph:
     def add_vertex(self, vertex):
         self.vertices[vertex] = set()
 
-    def add_edge(self, key, value):
-        if not self.vertices[key] and not self.vertices[value]:
+    def add_edge(self, v1, v2):
+        if not self.vertices[v1] and not self.vertices[v2]:
             raise IndexError('Vertex does not exist.')
         else:
-            self.vertices[key].add(value)
-            self.vertices[value].add(key)
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
 
-    def add_directed_edge(self, key, value):
-        if not self.vertices[key] and not self.vertices[value]:
+    def add_directed_edge(self, v1, v2):
+        if not self.vertices[v1] and not self.vertices[v2]:
             print('error: no vertext')
         else:
-            self.vertices[key].add(value)
+            self.vertices[v1].add(v2)
 
     def bft(self, start):
         queue = [start]
@@ -68,6 +68,34 @@ class Graph:
                 if v not in stack and v not in visited:
                     stack.append(v)
         print(f'PATH: {output}')
+
+    def recursive_dft(self, start, target, visited=set(), path=[]):
+        visited = set()
+        visited.add(start)
+        path += [start]
+        if start == target:
+            return path
+        for neighbor in self.vertices[start]:
+            if neighbor not in visited:
+                new_path = recursive_dft(neighbor, target, visited, path)
+                if new_path:
+                    return new_path
+        return None
+
+    def bfs(self, start, target):
+        queue = [start]
+        visited = set()
+        while len(queue) > 0:
+            path = queue.pop(0)  # path = [start]
+            v = path[-1]
+            if v not in visited:
+                if v == target:
+                    return path
+                visited.add(v)
+                for neighbor in self.vertices[v]:
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    queue.append(new_path)
 
 
 # [ ]
